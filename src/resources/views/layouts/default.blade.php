47c0f2ee681069192e6f2c8d8dbec3f8f6c91722
detectivecrow2540 <detectivecrow2540@gmail.com>
2021-04-19 23:02:06 +0900

    Update top image

--- src/resources/views/layouts/default.blade.php
+++ src/resources/views/layouts/default.blade.php
@@ -8,13 +8,10 @@
 </head>
 
 <body class="bg-gray-200">
-  {{-- <header>
-    <x-navbar />
-  </header> --}}
   <div id="app h-100">
     <section class="flex flex-col md:flex-row h-screen items-center">
       <div class="bg-indigo-600 hidden lg:block w-full md:w-1/2 xl:w-2/3 h-screen">
-        <img src="{{url('/images/top/top2.jpg')}}" alt="" class="w-full h-full object-cover">
+        <img src="{{url('/images/top/top.jpg')}}" alt="" class="w-full h-full object-cover">
       </div>
       <div
         class="bg-white w-full md:max-w-md lg:max-w-full md:mx-auto md:mx-0 md:w-1/2 xl:w-1/3 h-screen px-6 lg:px-16 xl:px-12 flex items-center justify-center">
