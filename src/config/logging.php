3592b819a597c4665a951030f32a013386df1acd
detectivecrow2540 <detectivecrow2540@gmail.com>
2021-07-28 00:07:08 +0900

    install log package (#57)

--- src/config/logging.php
+++ src/config/logging.php
@@ -99,6 +99,21 @@ return [
         'emergency' => [
             'path' => storage_path('logs/laravel.log'),
         ],
+
+        'cloudwatch' => [
+            'driver' => 'custom',
+            'via' => \App\Logging\CloudWatchLoggerFactory::class,
+            'sdk' => [
+                'region' => env('AWS_DEFAULT_REGION', 'ap-northeast-2'),
+                'version' => 'latest',
+                'credentials' => [
+                    'key' => env('AWS_ACCESS_KEY_ID'),
+                    'secret' => env('AWS_SECRET_ACCESS_KEY')
+                ]
+            ],
+            'retention' => env('CLOUDWATCH_LOG_RETENTION', 7),
+            'level' => env('CLOUDWATCH_LOG_LEVEL', 'error')
+        ],
     ],
 
 ];
