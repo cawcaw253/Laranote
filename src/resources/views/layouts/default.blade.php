f9e6f96840bc4fdb6874e0d9493dfe916b376cb3
detectivecrow2540 <detectivecrow2540@gmail.com>
2021-08-10 00:08:16 +0900

    error page (#69)

--- src/resources/views/layouts/default.blade.php
+++ src/resources/views/layouts/default.blade.php
@@ -8,9 +8,55 @@
 </head>
 
 <body class="bg-white h-screen">
-  @yield('content')
 
-  @stack('scripts')
+  <nav id="header" class="note-layout-body-header">
+    <div class="header-container">
+      <a class="title" href="{{ route('notes.index') }}">
+        LaraNote
+      </a>
+    </div>
+  </nav>
+
+  <div class="note-layout-body-contents">
+    <div id="app" class="note-container">
+      @yield('content')
+    </div>
+  </div>
+
 </body>
+
+<footer class="note-layout-body-footer">
+  <div class="footer-container">
+    <div class="contents">
+
+      <div class="contents-item">
+        <div>
+          <h3>About</h3>
+          <p>
+            LaraNote is my personal project based on laravel
+            you can email to me from <a href="mailto:" . {{ config('laranote.info.github') }}>Here</a>
+          </p>
+        </div>
+      </div>
+
+      <div class="contents-item">
+        <div>
+          <h3>Social</h3>
+          <ul>
+            <li>
+              <a href="{{ config('laranote.info.github') }}">my GitHub profile</a>
+            </li>
+            <li>
+              <a href="{{ config('laranote.info.instagram') }}">my Instagram</a>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </div>
+
+  </div>
+</footer>
+
+@stack('scripts')
 
 </html>
