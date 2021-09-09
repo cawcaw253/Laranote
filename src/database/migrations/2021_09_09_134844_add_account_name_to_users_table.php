6ed680c68f644496a5847f8fbd30441caba09d88
detectivecrow2540 <detectivecrow2540@gmail.com>
2021-09-09 22:55:24 +0900

    add account name to users table (#94)

---
+++ src/database/migrations/2021_09_09_134844_add_account_name_to_users_table.php
@@ -0,0 +1,42 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class AddAccountNameToUsersTable extends Migration
+{
+    const TABLE_NAME = 'users';
+
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        if (Schema::hasColumn(self::TABLE_NAME, 'account_name')) {
+            return;
+        }
+
+        Schema::table(self::TABLE_NAME, function (Blueprint $table) {
+            $table->string('account_name')->nullable()->unique();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        if (!Schema::hasColumn(self::TABLE_NAME, 'account_name')) {
+            return;
+        }
+
+        Schema::table('users', function (Blueprint $table) {
+            $table->dropColumn('account_name');
+        });
+    }
+}
